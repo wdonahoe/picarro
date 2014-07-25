0fe5dd7934772b083369a80ac21b5bb6950f4b85
wdonahoe <wdonahoe@udel.edu>
2014-07-25 14:25:04 -0400

    formatting

--- exp2.R
+++ exp2.R
@@ -22,9 +22,11 @@ DATE_FORMAT3 <- "%Y%m%d%H%M%S"
 #                cr  -- New line? Default = TRUE
 #
 printlog <- function( msg="", ..., ts=TRUE, cr=TRUE ) {
+
   if( ts ) cat( date(), " " )
   cat( msg, ... )
   if( cr ) cat( "\n")
+
 } # printlog
 
 # -----------------------------------------------------------------------------
@@ -33,43 +35,61 @@ printlog <- function( msg="", ..., ts=TRUE, cr=TRUE ) {
 #     Arguments: liblist -- A character vector of library names.
 # 
 loadlibs <- function( liblist ) {
+
   printlog( "Loading libraries..." )
   loadedlibs <- vector()
   for( lib in liblist ) {
+
     printlog( "Loading", lib )
     loadedlibs[ lib ] <- require( lib, character.only=T, warn.conflicts=F )
     if( !loadedlibs[ lib ] )
       warning( "this package is not installed!" )
+
   }
+
   invisible( loadedlibs )
+
 } # loadlibs
 
 non.na <- function( n, m ){
+
 	ifelse( is.na( n ) && is.na( m ), NA, TRUE )
+
 }
 
 proper_folder <- function( folder ){
+
 	posix <- non.na( strptime( folder,format=DATE_FORMAT1 ),
 					strptime( folder,format=DATE_FORMAT2 ) )
 	return ( posix )
+
 }
 
 proper_file <- function( file ){
+
 	file.split <- strsplit( file,"-" )
 	datetime <- paste0( file.split[[1]][2],file.split[[1]][3] )
+
 	return ( !is.na( strptime( datetime, format=DATE_FORMAT3 ) ) )
+
 }
 
 get_filenames <- function(){
+	
 	all <- lapply(list.files(INPUT_DIR, FOLDER_FORMAT),
 		function(f){ifelse(proper_folder(f),paste0(INPUT_DIR,SEP,f),NA)})
 	names <- unlist(all)
 	for (i in seq(1:length(all))){
+
 		f <- list.files(all[[i]],FILE_FORMAT)
+
 		for (j in seq(1:length(f))){
+
 			all[[i]][j] <- ifelse(proper_file(f[j]),paste0(names[i],"/",f[j]),NA)
+
 		}
 	}
+
 	return(all)
 }
 
@@ -88,8 +108,10 @@ main <- function() {
 	loadlibs(c("data.table"))
 
 	if( !file.exists( OUTPUT_DIR ) ) {
+
   		printlog( "Creating", OUTPUT_DIR )
   		dir.create( OUTPUT_DIR )
+
 	}
 
 	all_files <- get_filenames()
