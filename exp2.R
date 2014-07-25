aeb50544ab3a013f9786b7211568c19846f3d24d
wdonahoe <wdonahoe@udel.edu>
2014-07-25 13:20:16 -0400

    all files read

---
+++ exp2.R
@@ -0,0 +1,99 @@
+#!/usr/bin/env Rscript
+
+# William Donahoe, 2014
+
+args = commandArgs( trailingOnly = TRUE )
+
+INPUT_DIR <- args[ 1 ]
+SCRIPT_NAME <- "exp2.R"
+OUTPUT_DIR <- "out"
+FOLDER_FORMAT <- "(?:January|Febuary|March|April|May|June|July|August|September|October|November|December)_[0-9]{1,2}(_[0-9]{1,2}(AM|PM))?"
+FILE_FORMAT <- "JFAADS2012-20[0-9]{6}-\\d+-DataLog_User"
+SEP <- ifelse(Sys.info()['sysname'] != "Windows","/","\\")
+DATE_FORMAT1 <- "%B_%d_%I%p"
+DATE_FORMAT2 <- "%B_%d"
+DATE_FORMAT3 <- "%Y%m%d%H%M%S"
+
+# -----------------------------------------------------------------------------
+# printlog
+# Time-stamped output function
+#     Arguments: msg -- A string to print
+#                ts  -- Time stamp? Default = TRUE
+#                cr  -- New line? Default = TRUE
+#
+printlog <- function( msg="", ..., ts=TRUE, cr=TRUE ) {
+  if( ts ) cat( date(), " " )
+  cat( msg, ... )
+  if( cr ) cat( "\n")
+} # printlog
+
+# -----------------------------------------------------------------------------
+# loadlibs
+# Load a list of libraries
+#     Arguments: liblist -- A character vector of library names.
+# 
+loadlibs <- function( liblist ) {
+  printlog( "Loading libraries..." )
+  loadedlibs <- vector()
+  for( lib in liblist ) {
+    printlog( "Loading", lib )
+    loadedlibs[ lib ] <- require( lib, character.only=T, warn.conflicts=F )
+    if( !loadedlibs[ lib ] )
+      warning( "this package is not installed!" )
+  }
+  invisible( loadedlibs )
+} # loadlibs
+
+non.na <- function( n, m ){
+	ifelse( is.na( n ) && is.na( m ), NA, TRUE )
+}
+
+proper_folder <- function( folder ){
+	posix <- non.na( strptime( folder,format=DATE_FORMAT1 ),
+					strptime( folder,format=DATE_FORMAT2 ) )
+	return ( posix )
+}
+
+proper_file <- function( file ){
+	file.split <- strsplit( file,"-" )
+	datetime <- paste0( file.split[[1]][2],file.split[[1]][3] )
+	return ( !is.na( strptime( datetime, format=DATE_FORMAT3 ) ) )
+}
+
+get_filenames <- function(){
+	all <- lapply(list.files(INPUT_DIR, FOLDER_FORMAT),
+		function(f){ifelse(proper_folder(f),paste0(INPUT_DIR,SEP,f),NA)})
+	names <- unlist(all)
+	for (i in seq(1:length(all))){
+		f <- list.files(all[[i]],FILE_FORMAT)
+		for (j in seq(1:length(f))){
+			all[[i]][j] <- ifelse(proper_file(f[j]),paste0(names[i],"/",f[j]),NA)
+		}
+	}
+	return(all)
+}
+
+# TODO: Read all files into a data.table.
+read_files <- function( filenames ){
+
+}
+
+
+# --------------------------------------------------------------------------------
+# main
+main <- function() {
+
+	stopifnot( file.exists( INPUT_DIR ) )
+
+	loadlibs(c("data.table"))
+
+	if( !file.exists( OUTPUT_DIR ) ) {
+  		printlog( "Creating", OUTPUT_DIR )
+  		dir.create( OUTPUT_DIR )
+	}
+
+	all_files <- get_filenames()
+}
+
+main()
+
--- exp2.py
+++ exp2.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
 import subprocess
+import optparse
+import os
+import sys
+# TODO: Add Windows functionality.
+
+SCRIPT_NAME = "exp2.R"
+
 
 class ScriptException(Exception):
     def __init__(self, returncode, stdout, stderr, script):
@@ -8,21 +15,50 @@ class ScriptException(Exception):
         self.stdout = stdout
         self.stderr = stderr
         Exception.__init__('Error in script')
-        
+
+
 def run_bash(script):	
-	proc = subprocess.Popen(['bash','-c',script],
-		stdout = subprocess.PIPE, 
+	proc = subprocess.Popen(['bash','-c',script], 
 		stderr = subprocess.PIPE,
-		stdin = subprocess.PIPE)
+		stdin =  subprocess.PIPE)
 	stdout, stderr = proc.communicate()
 	if proc.returncode:
 		raise ScriptException(proc.returncode, stdout, stderr, script)
 	return stdout, stderr
 
+
+# TODO: Add date ranges.
+def run(foldername):
+	try:
+		subprocess.call(["./" + SCRIPT_NAME] + [foldername])
+	except OSError as e:
+		print "OS error({0}): {1}".format(e.errno, e.strerror)
+
+
 def main():
 	remove_spaces = "find . -depth -name \'* *\' \
 	 | while IFS= read -r f ; do mv -i \"$f\" \"$(dirname \"$f\")/$(basename \"$f\"|tr \' \' _)\" ; done"
-	run_bash(remove_spaces)
+
+	usage = "usage: ./%s foldername [options]" % os.path.basename(sys.argv[0])
+	parser = optparse.OptionParser(usage = usage)
+
+
+	parser.add_option('-s','--spaces',action="store_true",
+		dest="script",
+		help="remove spaces from all files and folders in [foldername]",
+		default=False)
+
+	(options, args) = parser.parse_args()
+
+	if len(args) < 1:
+		parser.error("Incorrect number of arguments.\n Please specify a folder name.")
+	elif len(args) > 2:
+		parser.error("Incorrect number of arguments.\n Too many arguments.")
+	else:
+		if options.script:
+			run_bash(remove_spaces)
+		run(args[0])
+
 
 if __name__ == "__main__":
 	main()
